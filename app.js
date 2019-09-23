c561362de2347467e4019648755ec768caf03aa4
RAVEN DELANEY <ravend17@gmail.com>
2019-09-23 14:36:04 -0700

    named variables

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function Product(name, image){
 
 //Random function
 function randomProduct(){
-  var random = Math.floor(Math.random() * Product.allImages.length)
+  var random = Math.floor(Math.random() * Product.allImages.length);
   return random;
 }
 
@@ -35,30 +35,44 @@ function renderProduct(){
     leftProduct = randomProduct();
     middleProduct = randomProduct();
     rightProduct = randomProduct();
-  } while(leftProduct === middleProduct || leftProduct === rightProduct || middleProduct === rightProduct)
+  } while(leftProduct === middleProduct || leftProduct === rightProduct || middleProduct === rightProduct);
 
   Product.allImages[leftProduct].view++;
   Product.allImages[middleProduct].views++;
   Product.allImages[rightProduct].views++;
 }
 
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
-new Product()
+if (prodVote === 25){
+  productImage.removeEventListener('click', handleClickOnProduct);
+  for(var i=0; i < Product.allImages.length; i++)
+  {
+    var product = Product.allImages[i];
+  }
+} else{
+  renderProduct();
+}
+
+new Product('bag', 'imgs/bag.jpg');
+new Product('banana', 'imgs/banana.jpg');
+new Product('bathroom', 'imgs/bathroom.jpg');
+new Product('boots', 'imgs/boots.jpg');
+new Product('breakfast', 'imgs/breakfast.jpg');
+new Product('bubblegum', 'imgs/bubblegum.jpg');
+new Product('chair', 'imgs/chair.jpg');
+new Product('cthulhu', 'imgs/cthulhu.jpg');
+new Product('dog-duck', 'imgs/dog-duck.jpg');
+new Product('dragon', 'imgs/dragon.jpg');
+new Product('pen', 'imgs/pen.jpg');
+new Product('pet-sweep', 'imgs/pet-sweep.jpg');
+new Product('scissors', 'imgs/scissors.jpg');
+new Product('shark', 'imgs/shark.jpg');
+new Product('sweep', 'imgs/sweep.jpg');
+new Product('tauntaun', 'imgs/tauntaun.jpg');
+new Product('unicorn', 'imgs/unicorn.jpg');
+new Product('usb', 'imgs/usb.gif');
+new Product('water-can', 'imgs/water-can.jpg');
+new Product('wine-glass', 'imgs/wine-glass.jpg');
+
+renderProduct();
+
+productImage.addEventListener('click', handleClickOnProduct);
