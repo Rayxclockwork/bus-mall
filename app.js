39f92602b5a9f8eada9237b69c52d1f3d91aab49
RAVEN DELANEY <ravend17@gmail.com>
2019-09-24 16:30:11 -0700

    added chart

--- app.js
+++ app.js
@@ -96,21 +96,22 @@ var handleClickOnProduct = function(event) {
   Product.allImages[leftProductIndex].view++;
   Product.allImages[middleProductIndex].view++;
   Product.allImages[rightProductIndex].view++;
-  makeList();
+  // makeList();
+  chart();
 };
 
 //creates list
-function makeList(){
-  var ob = Product.allImages;
-  var list = document.getElementById('results');
-  for (var j=0; j < Product.allImages.length; j++);{
+// function makeList(){
+//   var ob = Product.allImages;
+//   var list = document.getElementById('results');
+//   for (var j=0; j < Product.allImages.length; j++);{
 
-    var newLi = document.createElement('li');
-    newLi.textContent = Product.allImages.name;
-    list.appendChild(newLi);
-    console.log('test');
-  }
-}
+//     var newLi = document.createElement('li');
+//     newLi.textContent = Product.allImages.name;
+//     list.appendChild(newLi);
+//     console.log('test');
+//   }
+// }
 
 
 
@@ -129,6 +130,53 @@ function display(){
 }
 
 
+renderProduct();
 productImage.addEventListener('click', handleClickOnProduct);
 
-renderProduct();
+Product.objectName =[];
+Product.objectView = [];
+Product.objectClick = [];
+
+function data(){
+
+}
+
+function chart(){
+  var ctx = document.getElementById('busmallChart');
+  var Chart = new Chart(ctx,{
+    type:'bar',
+    data:{
+      labels: [Product.objectName],
+      datasets: [{
+        label: '# of Votes',
+        data: [Product.objectClick],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes:[{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
